78a86df2653a4d23e598653a4859fc69781d1e1b
James Quinlan <jquinlan@une.edu>
2023-06-26 09:58:00 -0400

    Update while.sh

--- sh/while.sh
+++ sh/while.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
+
 INPUT_STRING=hello
 while [ "$INPUT_STRING" != "bye" ]
 do
-	echo "Enter file name or 'bye' to quit"
-	read INPUT_STRING
-	echo "You typed: $INPUT_STRING"
-	touch $INPUT_STRING.php
+    echo "Enter file name or 'bye' to quit"
+    read INPUT_STRING
+    echo "You typed: $INPUT_STRING"
+    touch $INPUT_STRING.php
 done
